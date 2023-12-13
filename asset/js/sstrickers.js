19a29541c7e2d2d8afab4a722fd9fb600f091258
nassimb666 <bnassim750@gmail.com>
2023-12-13 14:56:21 +0100

    fetch + card

---
+++ asset/js/sstrickers.js
@@ -0,0 +1,23 @@
+
+fetch('../asset/data/stickers.json')
+    .then((response) => response.json())
+    .then((json) => {
+        json.forEach(element => {
+            const card = document.createElement('div');
+            card.classList.add('card');
+            card.innerHTML = `
+            <img src="../asset/img/${element.Images}" class="card-img-top stick" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Stickers</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+                    card's
+                    content.</p>
+                <a href="#" class="btn btn-primary">Acheter</a>
+            `
+            document.querySelector('#parent').appendChild(card)
+        });
+    });
+
+
+
+    
