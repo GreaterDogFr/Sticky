d2e89b2dd9cb90a6e84e812a7e6796d713d12797
nassimb666 <bnassim750@gmail.com>
2023-12-14 15:44:54 +0100

    update script

--- asset/js/sstrickers.js
+++ asset/js/sstrickers.js
@@ -1,23 +1,98 @@
-
-fetch('../asset/data/stickers.json')
+fetch('./asset/data/stickers.json')
     .then((response) => response.json())
     .then((json) => {
         json.forEach(element => {
             const card = document.createElement('div');
             card.classList.add('card');
             card.innerHTML = `
+                <img src="../asset/img/${element.Images}" class="card-img-top stick" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p class="card-text">${element.Price}€</p>
+                    <a href="#" class="btn btn-primary">Acheter</a>
+                    <button class="btn btn-light"><i class="bi bi-suit-heart"></i> Favoris</button>
+                </div>`;
 
-            <img src="../asset/img/${element.Images}" class="card-img-top stick" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${element.name}</h5>
-                <p class="card-text">${element.Price}€</p>
-                <a href="#" class="btn btn-primary">Acheter</a>
-            `
-            
-            document.querySelector('#parent').appendChild(card)
+            // Ajoutez un gestionnaire d'événements pour le clic sur l'image
+            card.addEventListener('click', function () {
+                // Ouvrez une nouvelle page HTML générée en JavaScript
+                const newPage = window.open('', '_blank');
+                newPage.document.write(`
+                    <html>
+                        <head>
+                            <meta charset="UTF-8">
+                            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+                                integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+                            <link rel="stylesheet" href="asset/css/style.css">
+                            <link rel="stylesheet" href="./asset/css/styleproduit.css">
+                        </head>
+                        <body>
+                            <nav class="navbar navbar-expand-md">
+                                <div class="container-fluid">
+                                    <a class="navbar-brand" href="#">Sticky</a>
+
+                                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                                    aria-label="Toggle navigation">
+                                    <span class="navbar-toggler-icon"></span>
+                                </button>
+                                <div class="collapse navbar-collapse spacenav" id="navbarSupportedContent">
+                                    <ul class="navbar-nav">
+                                        <li class="nav-item">
+                                            <a class="nav-link active" aria-current="page" href="index.html">Accueil</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="Produit.html">Nos Stickers</a>
+                                        </li>
+                                        <li class="nav-item">
+                                            <a class="nav-link" href="#">Stickers Personalisé</a>
+                                        </li>
+                                    </ul>
+                                    <!-- <button>Panier</button> -->
+                                    <button class="btn btn-outline nav-item cartButton" type="button" data-bs-toggle="offcanvas"
+                                        data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
+                                        Panier
+                                    </button>
+                                </div>
+                            </div>
+                        </nav>
+                        <!-- Offcanvas -->
+                        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+                            <div class="offcanvas-header">
+                                <h5 class="offcanv Fas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <div>
+                                    Some text as placeholder. In real life you can have the elements you have chosen. Like,
+                                    text, images, lists, etc.
+                                </div>
+                            </div>
+                        </div>
+                        <title>${element.name}</title>
+                            <img src="../asset/img/${element.Images}" class="card-img-top stick" alt="...">
+                            <p>Prix: ${element.Price}€</p>
+                            <p>Description: ${element.description}</p>
+                            
+                            <footer class="bottom py-3">
+                                <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">Accueil</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">A propos de nous</a></li>
+                                <li class="nav-item"><a href="#" class="nav-link px-2 text-body-secondary">FAQ</a></li>
+                                </ul>
+                                <p class="text-center text-body-secondary">© 2023 Sticky, Inc</p>
+                            </footer>
+                            <script src="../asset/js/sstrickers.js"></script>
+                            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+                                integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+                                crossorigin="anonymous"></script>
+                        </body>
+
+                    </html>
+                `);
+            });
+
+            document.querySelector('#parent').appendChild(card);
         });
     });
-
-
-
-    
